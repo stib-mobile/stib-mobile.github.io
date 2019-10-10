e08c7fe1b74e769d9d45d72c951ccb9c484c7af9
Nguyendanghung0201 <danghung020195@gmail.com>
2019-10-10 09:23:37 +0700

    last commit

--- javascript/index.js
+++ javascript/index.js
@@ -1,14 +1,5 @@
 $(document).ready(function () {
 
-<<<<<<< HEAD
-    var header_top = $('#header').offset().top + 60;
-    if ((header_top < 200) && ($(window).width() > 1000)) {
-        $("#logo-header").hide("slow")
-        
-    } else {
-        $("#logo-header").show("slow")
-    }
-=======
 
     var media = $('.video-tut').get(0);
     $('#modelId').on('hide.bs.modal', function () {
@@ -24,18 +15,13 @@ $(document).ready(function () {
     //     media.play();
     // })
     
->>>>>>> master
 
     var header_top = $('#header').offset().top + 60;
 
     if (header_top > 60) {
         $("#change-color ul li a").css("color", "#f39c12")
-<<<<<<< HEAD
-   }
-=======
 
     }
->>>>>>> master
     $(window).scroll(function () {
         var vitrihientai = window.pageYOffset;
         // console.log(vitrihientai);
@@ -44,33 +30,11 @@ $(document).ready(function () {
         var page2 = page1 + $("#page1").outerHeight(true);
         var distance = $('#show').offset().top;
         var video_1 = $('#video1').offset().top;
-<<<<<<< HEAD
-
         var video_2 = $('#video2').offset().top;
-
-        // var video_3 = $('#video3').offset().top;
-
-        // var video_main = document.getElementById("video")
-=======
-        var video_2 = $('#video2').offset().top;
->>>>>>> master
         var video1 = document.getElementById("video1")
         var video2 = document.getElementById("video2")
         var distance3 = distance + 250;
         var header_top = $('#header').offset().top + 60;
-<<<<<<< HEAD
-
-        // if ((header_top + 200) > video && header_top <= (video + 100)) {
-
-        //     video_main.play()
-
-        // } else {
-        //     video_main.pause();
-        // }
-        // /////// page 1
-
-=======
->>>>>>> master
         if ((header_top + 300) > video_1 && header_top <= (video_1 + 50)) {
 
             video1.play()
@@ -86,28 +50,6 @@ $(document).ready(function () {
         } else {
             video2.pause();
         }
-<<<<<<< HEAD
-        // page 3 //////////
-        // if ((header_top + 300) > video_3 && header_top <= (video_3 + 50)) {
-
-        //     video3.play()
-
-        // } else {
-        //     video3.pause();
-        // }
-        // //////////////////////////////////////////////////////////////////////////////////////////////////// change background color for header
-        if (header_top == 60) {
-            $("#change-color ul li a").css("color", "white")
-
-            $("#header").css("background", "none")
-
-        } else if (1 == 1 | (header_top > 60 && header_top < distance) | (header_top >= page1 && header_top < page2)) {
-            $("#header").css("background-color", "white")
-            $("#change-color ul li a").css("color", "#f39c12")
-
-        } else if ((header_top >= distance && header_top <= page1) | (header_top >= page2)) {
-            $("#header").css("background-color", "#10102d")
-=======
 
         if (vitrihientai > 100) {
             $('#header').addClass('bg-active');
@@ -119,7 +61,6 @@ $(document).ready(function () {
         }
         if (vitrihientai > 300) {
             $('#to-top').addClass('active');
->>>>>>> master
         }
         else {
             $('#to-top').removeClass('active');
@@ -153,96 +94,6 @@ $(document).ready(function () {
         $("#menu_show").fadeToggle("slow");
     });
     $("#menu_show_icon_close").click(function () {
-<<<<<<< HEAD
-         $("#menu_show").fadeToggle("slow")
-
-    })
-
-    // 
-    var page1 = $('#page1').offset().top;
-    var page2 = page1 + $("#page1").outerHeight(true);
-    // var page3 = $('#page3').offset().top;
-    var distance = $('#show').offset().top;
-    // var download = $('#download').offset().top;
-    // var video = $('#video').offset().top;
-    var video_1 = $('#video1').offset().top;
-
-    var video_2 = $('#video2').offset().top;
-
-    // var video_3 = $('#video3').offset().top;
-
-    // var video_main = document.getElementById("video")
-    var video1 = document.getElementById("video1")
-    var video2 = document.getElementById("video2")
-    // var video3 = document.getElementById("video3")
-    var distance3 = distance + 250;
-
-    var header_top = $('#header').offset().top + 60;
-
-    // if ((header_top + 200) > video && header_top <= (video + 100)) {
-
-    //     video_main.play()
-
-    // } else {
-    //     video_main.pause();
-    // }
-    // /////// page 1
-
-    if ((header_top + 300) > video_1 && header_top <= (video_1 + 50)) {
-
-        video1.play()
-
-    } else {
-        video1.pause();
-    }
-    //  page 2///////
-    if ((header_top + 300) > video_2 && header_top <= (video_2 + 50)) {
-
-        video2.play()
-
-    } else {
-        video2.pause();
-    }
-    // page 3 //////////
-    // if ((header_top + 300) > video_3 && header_top <= (video_3 + 50)) {
-
-    //     video3.play()
-
-    // } else {
-    //     video3.pause();
-    // }
-    // //////////////////////////////////////////////////////////////////////////////////////////////////// change background color for header
-    if (header_top == 60) {
-        $("#change-color ul li a").css("color", "white")
-
-        $("#header").css("background", "none")
-
-    } else if ((header_top > 60 && header_top < distance) | (header_top >= page1 && header_top < page2)) {
-        $("#header").css("background-color", "white")
-        $("#change-color ul li a").css("color", "#f39c12")
-
-    } else if ((header_top >= distance && header_top <= page1) | (header_top >= page2)) {
-        $("#header").css("background-color", "#10102d")
-    }
-    // show logo
-  
-
-    // ////////////////////////////////////// animation 
-    if (header_top >= distance && header_top < distance3) {
-        $("#photo_animation").css("animation-play-state", "running")
-        $(".app_provide .detail2").css("animation-play-state", "running")
-    } else if (header_top >= distance3) {
-        $(".app_provide .detail3").css("animation-play-state", "running")
-
-    } else {
-        $("#photo_animation").css("animation-play-state", "paused")
-    }
-    // video play
-    $("#show_video_download").click(function () {
-        $("#video").show()
-    })
-
-=======
 
         $("#menu_show").fadeToggle("slow");
 
@@ -286,5 +137,4 @@ $(document).ready(function () {
         }
         $('html').animate({ scrollTop: $('#contact-us').offset().top - 60 }, 500);
     })
->>>>>>> master
 });
